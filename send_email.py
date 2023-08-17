45277f58a2c7e7856aaee8b5dcf3258a5568cee9
NicolasTorres11 <99929882+NicolasTorres11@users.noreply.github.com>
2023-08-17 15:14:06 -0500

    master

--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib
+import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
@@ -9,15 +10,33 @@ smtp_username = 'asistemas@berlinasdelfonce.com'
 smtp_password = 'NUEVO*2023'
 sender_mail = 'asistemas@berlinasdelfonce.com'
 receiver_mail = 'asistemas@berlinasdelfonce.com'
-subject = 'PRUEBA'
+subject = 'BERLINAS DEL FONCE S.A. --> Informe de Vencimientos de SOAT, RTM y Tarjetas de Operación'
 
+dia_registro = datetime.datetime.now()
 
 msg = MIMEMultipart()
 msg['From'] = sender_mail
 msg['To'] = receiver_mail
 msg['Subject'] = subject
 
-body = 'PRUEBA ENVIO PDF'
+body = f"""
+        <p>INFORME DE VENCIMIENTOS DE SOAT, RTM Y LAS TARJETAS DE OPERACION EN LOS VEHICULOS DE BARRANQUILLA</p>
+        <br/>
+        Adjunto al presente estamos enviando el detalle del Informe de Vencimientos de SOAT, RTM y Tarjetas de Operación:<br/>
+        <br/>
+        Fecha de Movimiento: {dia_registro:%Y-%m-%d %H:%M:%S}<br/>
+        <br/>
+        Cordialmente,<br/>
+        <br/>
+        Monica Pulido Molina<br/>
+        Asistente Administrativa<br/>
+        <a href="mailto:tramitesvehiculos@berlinasdelfonce.com">tramitesvehiculos@berlinasdelfonce.com</a><br/>
+        <a href="http://www.berlinasdelfonce.com">www.berlinasdelfonce.com</a><br/>
+        Teléfono: (60-1) 7435050 Ext 1201<br/>
+        Oficina Principal: Cra 68D No. 15 - 15<br/>
+        Bogota-Colombia<br/>
+        """
+msg.attach(MIMEText(body, "plain"))
 
 pdf_filename = 'vencimientosBarranquilla.pdf'
 
