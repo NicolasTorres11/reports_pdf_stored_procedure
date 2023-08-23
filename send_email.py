0335b7b5264dfc752633a3b92a0a7448462035a2
NicolasTorres11 <99929882+NicolasTorres11@users.noreply.github.com>
2023-08-23 07:30:04 -0500

    master

--- send_email.py
+++ send_email.py
@@ -5,24 +5,26 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
 
-def send_email(pdf_filename):
+def send_email(pdf_filename, ciudad, recipient_emails):
     smpt_server = 'mail.berlinasdelfonce.com'
     smtp_port = 465
     smtp_username = 'asistemas@berlinasdelfonce.com'
     smtp_password = 'NUEVO*2023'
     sender_mail = 'asistemas@berlinasdelfonce.com'
-    receiver_mail = 'asistemas@berlinasdelfonce.com'
+    # receiver_mail = 'asistemas@berlinasdelfonce.com'
+    # receiver_2 = 'sistemas@berlinasdelfonce.com'
     subject = 'BERLINAS DEL FONCE S.A. --> Informe de Vencimientos de SOAT, RTM y Tarjetas de Operación'
 
     dia_registro = datetime.datetime.now()
 
     msg = MIMEMultipart()
     msg['From'] = sender_mail
-    msg['To'] = receiver_mail
+    recipients = ', '.join(recipient_emails)
+    msg['To'] = recipients
     msg['Subject'] = subject
 
     body = f"""
-            <p>INFORME DE VENCIMIENTOS DE SOAT, RTM Y LAS TARJETAS DE OPERACION EN LOS VEHICULOS DE BOGOTA</p>
+            <p>INFORME DE VENCIMIENTOS DE SOAT, RTM Y LAS TARJETAS DE OPERACION EN LOS VEHICULOS DE {ciudad}</p>
             <br/>
             Adjunto al presente estamos enviando el detalle del Informe de Vencimientos de SOAT, RTM y Tarjetas de Operación:<br/>
             <br/>
@@ -50,12 +52,18 @@ def send_email(pdf_filename):
     try:
         with smtplib.SMTP_SSL(smpt_server, smtp_port) as server:
             server.login(smtp_username, smtp_password)
-            server.sendmail(sender_mail, receiver_mail, msg.as_string())
+            server.sendmail(sender_mail, recipient_emails, msg.as_string())
         print('Correo Enviado Con exito!!!')
     except Exception as e:
         print('Error al Enviar el Correo:', e)
 
 
-pdf_filename = 'vencimientosBogota.pdf'
+data = [
+    ('vencimientosBarranquilla.pdf', 'BARRANQUILLA', [
+     'asistemas@berlinasdelfonce.com', 'penatorresnicolas@gmail.com']),
+    ('vencimientosBogota.pdf', 'BOGOTA', [
+     'sistemas@berlinasdelfonce.com', 'penatorresnicolas@gmail.com'])
+]
 
-send_email(pdf_filename)
+for pdf_file, city, recipient_email in data:
+    send_email(pdf_file, city, recipient_email)
